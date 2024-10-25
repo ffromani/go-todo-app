4380c3bfce7cc375dd2bc564383a47126e2be186
Federico Paolinelli <fpaoline@redhat.com>
2024-10-25 21:43:38 +0200

    Controller: add a benchmark to validate the different alternatives
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ controller/controller_test.go
@@ -0,0 +1,38 @@
+package controller
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ffromani/go-todo-app/model"
+)
+
+// exercise
+
+func BenchmarkTodoFromRequest(b *testing.B) {
+	todo := model.New("todo")
+	todo.Assign("fede")
+	todo.Describe("hello")
+
+	serialized, err := todo.Serialize()
+	if err != nil {
+		panic("")
+	}
+
+	body := bytes.NewReader(serialized)
+	for i := 0; i < b.N; i++ {
+		body.Seek(0, io.SeekStart)
+		req := httptest.NewRequest(http.MethodGet, "/foo", body)
+		_, code, err := todoFromRequest(req)
+		// _, code, err := todoFromRequestReader(req)
+		if err != nil {
+			b.Fatal("error", err)
+		}
+		if code != 0 {
+			b.Fatal("error code", code)
+		}
+	}
+}
