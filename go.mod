2d57e79ebcffaf2baedfda3d6dcf57bf0da31a12
Federico Paolinelli <fedepaol@gmail.com>
2024-10-26 10:13:58 +0200

    Storage: implement redis interaction
    Filling the implementation.

--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/ffromani/go-todo-app
 
 go 1.22.7
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/redis/go-redis/v9 v9.7.0
+)
 
-require github.com/davecgh/go-spew v1.1.1 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
+
+require github.com/davecgh/go-spew v1.1.1
--- store/redis.go
+++ store/redis.go
@@ -1,30 +1,74 @@
 package store
 
-type Redis struct{}
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
 
 var _ Storage = &Redis{}
 
-func NewRedis() (*Redis, error) {
-	return nil, nil
+type Redis struct {
+	rdb *redis.Client
+}
+
+func NewRedis(url, password string) (*Redis, error) {
+	return &Redis{
+		rdb: redis.NewClient(&redis.Options{
+			Addr:     url,
+			Password: password,
+			DB:       0, // use default DB
+		}),
+	}, nil
 }
 
 func (rd *Redis) Close() error {
-	return nil
+	return rd.rdb.Close()
 }
 
 func (rd *Redis) Create(data Blob, id ID) error {
+	err := rd.rdb.Set(context.Background(), string(id), data, 0).Err()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (rd *Redis) LoadAll() ([]Item, error) {
-	return nil, nil
+	ctx := context.Background()
+	iter := rd.rdb.Scan(ctx, 0, "", 0).Iterator()
+	res := []Item{}
+	for iter.Next(ctx) {
+		key := iter.Val()
+		val, _ := rd.rdb.Get(ctx, key).Result()
+		res = append(res, Item{ID: ID(key), Blob: Blob(val)})
+	}
+	return res, nil
 }
 
 func (rd *Redis) Load(id ID) (Blob, error) {
-	return nil, nil
+	data, err := rd.rdb.Get(context.Background(), string(id)).Result()
+	if err == redis.Nil {
+		return nil, fmt.Errorf("id %s does not exist", id)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return Blob(data), nil
 }
 
 func (rd *Redis) Save(id ID, blob Blob) error {
+	// Non thread safe!
+	data, err := rd.rdb.Get(context.Background(), string(id)).Result()
+	if err == redis.Nil {
+		return fmt.Errorf("id %s does not exist", id)
+	}
+
+	err = rd.rdb.Set(context.Background(), string(id), data, 0).Err()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
